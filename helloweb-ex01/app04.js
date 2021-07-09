b2cc022b958438f38d7d43cc13a9c4403265ffbd
sinhu-jung <tlsgn26@naver.com>
2021-07-09 15:55:39 +0900

    server application

--- helloweb-ex01/app04.js
+++ helloweb-ex01/app04.js
@@ -6,14 +6,47 @@ const port = 8080;
 const app = connect();
 app.use(serveStatic(__dirname + '/public'));
 app.use(connectRoute(function(router){
-    router.get("/", function(req, res){
-        res.writeHead(200, {
+    router.get("/", function(req, resp){
+        resp.writeHead(200, {
             'Content-Type': "text/html"
         });
-        res.end("<h1>main</h1")
+        resp.end("<h1>main</h1>");
+    });
+    router.get("/user", function(req, resp){
+        console.log(req._parsedUrl.query);
+
+        req.query = {};
+        params = (req._parsedUrl.query || "").split("&");
+        params.forEach(function(param){
+            tokens = param.split("=");
+            req.query[tokens[0]] = tokens[1];
+        });
+
+        
+        resp.writeHead(200, {
+            'Content-Type': "text/html"
+        });
+        resp.end("<h1>user no:" + req.query.no + "</h1>");
+    });
+    router.get("/guestbook", function(req, resp){
+        resp.writeHead(200, {
+            'Content-Type': "text/html"
+        });
+        resp.end("<h1>guestbook list</h1>");
+    });
+    router.get("/board", function(req, resp){
+        resp.writeHead(200, {
+            'Content-Type': "text/html"
+        });
+        resp.end("<h1>board list</h1>");
+    });
+    router.get("/board/:no", function(req, resp){
+        resp.writeHead(200, {
+            'Content-Type': "text/html"
+        });
+        resp.end("<h1>board view(" + req.params.no + ")</h1>");
     });
 }));
-
 app.listen(port, function(){
     console.log(`Http Server running on port ${port}`);
-})
+});

222668938102447e50298952742b2d56b1b761fe
sinhu-jung <tlsgn26@naver.com>
2021-07-09 12:53:39 +0900

    모듈 테스트

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,8 @@
+/*
+sinhu-math.js 파일 모듈 테스트 
+*/
+const shMath = require('../sinhu-math/sinhu-math')
+
+console.log(shMath.sum(1,2,3,4));
+console.log(shMath.max(-10, -20, -30, -40));
+console.log(shMath.min(-10, -20, -30, -40));

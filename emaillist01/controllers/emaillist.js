96a01ff66e1257d7ddb1d9a3839fb0dcc984daa4
sinhu-jung <tlsgn26@naver.com>
2021-07-13 11:47:45 +0900

    model #2

--- emaillist01/controllers/emaillist.js
+++ emaillist01/controllers/emaillist.js
@@ -3,8 +3,7 @@ const model = require('../models/emaillist');
 module.exports = {
     index: async function(req, res) {
         const results = await model.findAll();
-        console.log(results);
-        res.render('index');
+        res.render('index', {list:results || []});
     },
     form: function(req, res) {
         res.render('form');
--- emaillist01/models/emaillist.js
+++ emaillist01/models/emaillist.js
@@ -1,23 +1,15 @@
 const mysql = require('mysql');
+const util = require('util');
 
 const dbconn = require('./dbconn');
+
 module.exports = {
     findAll: async function() {
         const conn = dbconn();
-        const query = function(sql, data){
-            return new Promise(function(resolve, reject){
-                conn.query(
-                    sql,
-                    data,
-                    function(error, rows, field) {
-                    });                
-            });
-        }
-
+        //const query = (sql, data) => new Promise((resolve, reject) => conn.query(sql, data, (error, rows, field) => error ? reject(error) : resolve(rows)));
+        const query = util.promisify(conn.query).bind(conn);
         try {
-            /*
-            */
-            const results = await query("select first_name, last_name, email from emaillist order by no desc", []);
+            const results = await query("select first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
             return results;    
         } catch(e) {
             console.error(e);
@@ -25,6 +17,7 @@ module.exports = {
             conn.end();
         }
     },
+
     insert: function() {
     }
 }
